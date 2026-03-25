d100c7801d0d45acb90fb91620247e182d5d553b
rak-mi <someonex666@gmail.com>
2026-03-25 19:12:58 -0400

    add lodestone parsing

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import { Character } from "@xivapi/nodestone";
+
+const parser = new Character();
+
+const character = await parser.parse({ params: { characterId: "31610743" } });
+console.log(JSON.stringify(character, null, 2));
